4dcaa167cee1164cd3b0145412724682627c6ab1
SubZeroPL <subzeropl@gmail.com>
2025-01-15 10:27:01 +0100

    fix: some code cleanups

--- Classes/Functions.cs
+++ Classes/Functions.cs
@@ -59,22 +59,6 @@ public static class Functions
             throw new ProcessingException(string.Join(Environment.NewLine, error));
         }
 
-        /*
-            [0]: "chdman - MAME Compressed Hunks of Data (CHD) manager 0.257 (mame0257)"
-            [1]: "Input file:   G:\\test\\psx\\Tekken 3 (USA)\\Tekken 3 (USA).chd"
-            [2]: "File Version: 5"
-            [3]: "Logical size: 716,382,720 bytes"
-            [4]: "Hunk Size:    19,584 bytes"
-            [5]: "Total Hunks:  36,580"
-            [6]: "Unit Size:    2,448 bytes"
-            [7]: "Total Units:  292,640"
-            [8]: "Compression:  cdlz (CD LZMA), cdzl (CD Deflate), cdfl (CD FLAC)"
-            [9]: "CHD size:     459,654,890 bytes"
-            [10]: "Ratio:        64.2%"
-            [11]: "SHA1:         319ec7377eb0c3ef0b6049e440937d26ae743290"
-            [12]: "Data SHA1:    2685e94ac98b12f8771db38051f1c2622f0bbc7a"
-         */
-
         var matches = Constants.ChdManVersionRegex().Matches(output[0]);
         game.ChdData.ChdManVersion = matches[0].Groups[1].Value;
         var version = int.Parse(output[2].Split(':').Last().Trim());
@@ -268,7 +252,7 @@ public static class Functions
                 return;
             }
             var lib = Assembly.LoadFrom("JAssOn.dll");
-            Logger.Debug($"version: {lib.GetName()?.Version?.ToString(3)}");
+            Logger.Debug($"version: {lib.GetName().Version?.ToString(3)}");
             var cls = lib.GetType("JAssOn.Ldrr");
             if (cls == null) return;
             dynamic? inst = Activator.CreateInstance(cls);
